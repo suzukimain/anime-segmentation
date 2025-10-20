72458d0c02b51d9489242e91e99638ed4098a756
suzukimain <131413573+suzukimain@users.noreply.github.com>
2025-10-20 13:58:53 +0900

    Fix relative import paths in __init__.py for convert_img and AnimeSegmentation

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from config.convert import convert_img  # type: ignore
+from .config.convert import convert_img  # type: ignore
 
-from config.train import AnimeSegmentation
+from .config.train import AnimeSegmentation
 
 __all__ = ["convert_img", "AnimeSegmentation"]
